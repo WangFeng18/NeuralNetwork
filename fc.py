1f7d2e6802f44033e96d660219e6afe6b21c75e2
WangFeng18 <wangfengwork17@gmail.com>
2018-09-22 18:21:27 +0800

    update

--- fc.py
+++ fc.py
@@ -35,3 +35,17 @@ class MSELoss(object):
     
     def backward(self):
         self.gradient_wrt_input = 2*(self.data-self.gt)/self.n_data_elements
+
+class Sigmoid(object):
+    def __init__(self):
+        pass
+
+    def forward(data):
+        self.data = data
+        self.output = 1./(1+np.exp(-self.data))
+        return self.output
+
+    def backward(gradient_wrt_output):
+        self.gradient_wrt_input = self.output * (1. - self.output)
+        return self.gradient_wrt_input
+
